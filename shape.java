c5431b3a046d423def5d4fdf6269c26313f1da78
S MOHAMMAD ASHRAF <ashrafali31033@gmail.com>
2024-12-06 14:08:46 +0530

    Add files via upload

--- shape.java
+++ shape.java
@@ -1,18 +1,16 @@
-import java.util.*;
-public class shape {
-    public static void main(String[] args) {
+public class shape
+{
+    public static void main(String args[])
+    {
+        int n= 5;
 
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-
-        for(int i=1;i<=n;i++)
+        for(int i =1;i<=n;i++)
         {
             for(int j =1;j<=i;j++)
             {
-                System.out.println((char)(64+j)+" ");
+                System.out.print(i+" ");
             }
-        }
         System.out.println();
+        }
     }
-    
 }
